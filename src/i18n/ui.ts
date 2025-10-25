ddc4535a401714bb73c7ebd1dbf31631b46406f2
migue10david <miguedeveloper23@gmail.com>
2025-10-25 11:02:10 -0400

    name changed

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -11,7 +11,7 @@ export const ui = {
     hero_bubble: "Autentic tours in Cuba",
     hero_title1: "Discover",
     cuba: "Cuba",
-    hero_title2: "like never before with Excursiones Cuba! ",
+    hero_title2: "like never before with Cuba Easy Tours!",
     hero_subtitle:
       "We offer the most authentic tours of Havana, Matanzas, Varadero, Trinidad, and other cities. Enjoy a unique journey aboard iconic American classic cars, while a native Cuban guide, full of passion and knowledge, shares the true story of our island. Personalized and flexible excursions, specially designed for you, with comfortable transportation and that special touch only Cubans can offer. Experience the essence of Cuba in every corner!",
     whatsapp: "Book through WhatsApp",
@@ -32,7 +32,7 @@ export const ui = {
     hero_bubble: "Аутентичные туры на Кубу",
     hero_title1: "Открой",
     cuba: "Кубy",
-    hero_title2: "с нами, как никогда раньше, с Excursiones Cuba !",
+    hero_title2: "с нами, как никогда раньше, с Cuba Easy Tours!",
     hero_subtitle:
       "Мы предлагаем самые аутентичные туры в Гавану, Матансас, Варадеро, Тринидад и другие города.Отправьтесь в уникальное путешествие на ретро-американских автомобилях в сопровождении настоящего местного кубинского гида, который с любовью и мастерством расскажет вам о реальной истории нашего острова.Индивидуальные и адаптированные экскурсии, созданные специально для вас, с комфортным транспортом и особым шармом, который могут подарить только кубинцы. Почувствуйте душу Кубы в каждом уголке!",
     whatsapp: "Забронировать через WhatsApp",
