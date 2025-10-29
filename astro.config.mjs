a02eafe1eb1e5174ce330fa8d7f43387870b6632
malaudiaz <113136614+malaudiaz@users.noreply.github.com>
2025-10-29 10:23:46 -0400

    metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'http://localhost:4321',
   vite: {
-  plugins: [tailwindcss()],
-},
-
-  i18n:{
-    defaultLocale: "ru",
-    locales: ['en', 'ru'],
+    plugins: [tailwindcss()],
   },
 
-  integrations: [react()]
+  i18n: {
+    defaultLocale: "ru",
+    locales: ["en", "ru"],
+  },
+
+  integrations: [react()],
 });
