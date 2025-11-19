14930db9b5eac5199d70bd1814b760d0c4e2bc54
migue10david <miguedeveloper23@gmail.com>
2025-11-18 19:59:21 -0500

    new seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://localhost:4321',
+  site: 'https://cubaeasytours.com',
   vite: {
     plugins: [tailwindcss()],
   },
